65f48d9338d3b3b1225209bea1c2993c765568b2
Dan Parnham <dan@emergent-design.co.uk>
2024-03-28 10:12:42 +0000

    Implemented a comparison utility - it can compare two trees (or two entities) and return a list of the diffs.

--- include/entity/utilities/compare.hpp
+++ include/entity/utilities/compare.hpp
@@ -1,34 +1,73 @@
 #pragma once
 
+#include <entity/tree.hpp>
+#include <entity/entity.hpp>
+
 
 namespace ent
 {
+	class compare
+	{
+		public:
 
+			struct diff
+			{
+				std::string level;
+				ent::tree before;
+				ent::tree after;
+
+				emap(eref(level), eref(before), eref(after))
+			};
+
+
+			// Compare two trees and document the changes. It will skip paths that exist in only one of the trees.
+			static std::vector<diff> trees(const tree &original, const tree &updated)
+			{
+				std::vector<diff> changes;
+
+				recurse("", original, updated, changes);
+
+				return changes;
+			}
+
+
+			// Compare two entities and document the changes. It will skip paths that exist in only one of the trees.
+			template <typename T> static std::vector<diff> entities(const T &original, const T &updated)
+			{
+				std::vector<diff> changes;
+
+				recurse("", to_tree(original), to_tree(updated), changes);
+
+				return changes;
+			}
+
+
+		private:
+
+			static void recurse(const string level, const tree &original, const tree &updated, std::vector<diff> &changes)
+			{
+				for (auto &[name, before] : original.children)
+				{
+					if (!updated.contains(name))
+					{
+						continue;
+					}
+
+					bool differ = false;
+					auto &after = updated.at(name);
+
+					switch (before.get_type())
+					{
+						case tree::Type::Object:	recurse(level + name + ":", before, after, changes);	break;
+						case tree::Type::Array:		differ = before.as_array() != after.as_array();			break;
+						default:					differ = before != after;								break;
+					}
+
+					if (differ)
+					{
+						changes.push_back({ level + name, before, after });
+					}
+				}
+			}
+	};
 }
-
-
-// static void Changes(const string level, const ent::tree &original, const ent::tree &updated, std::vector<ent::tree> &changes)
-// 		{
-// 			for (auto &k : key_iterator(original.children))
-// 			{
-// 				if (!updated.contains(k)) continue;
-
-// 				bool differ = false;
-
-// 				switch (original.at(k).get_type())
-// 				{
-// 					case ent::tree::Type::Object:	Changes(level + k + ":", original.at(k), updated.at(k), changes);	break;
-// 					case ent::tree::Type::Array:	differ = original.at(k).as_array() != updated.at(k).as_array();		break;	// For some reason comparing the trees directly does not work in this module, even though it worked in all of the libentity test cases I could think of.
-// 					default:						differ = original.at(k) != updated.at(k);							break;
-// 				}
-
-// 				if (differ)
-// 				{
-// 					changes.push_back({
-// 						{ "field", level + k },
-// 						{ "original", original.at(k) },
-// 						{ "new", updated.at(k) }
-// 					});
-// 				}
-// 			}
-// 		}
--- src/test/utilities.cpp
+++ src/test/utilities.cpp
@@ -1,5 +1,6 @@
 #include "doctest.h"
 #include <entity/utilities/base64.hpp>
+#include <entity/utilities/compare.hpp>
 #include <map>
 
 using namespace std;
@@ -34,4 +35,57 @@ TEST_CASE("base64 test vectors") //, "[utilities]")
 		}
 	}
 }
+
+TEST_CASE("trees and entities can be compared")
+{
+	SUBCASE("trees")
+	{
+		const auto before = tree()
+			.set("string", "string value")
+			.set("integer", 42)
+			.set("floating", 3.14)
+			.set("boolean", true)
+			.set("array", vector<tree> { 1, 2, 3 })
+			.set("tree", tree().set("name", "child"));
+
+		const auto after = tree(before)
+			.set("integer", 8)
+			.set("only", "here")
+			.set("tree", tree().set("name", "another"));
+
+		auto diffs = compare::trees(before, after);
+
+		CHECK(diffs.size()					== 2);
+		CHECK(diffs[0].level				== "integer");
+		CHECK(diffs[0].before.as_long() 	== 42);
+		CHECK(diffs[0].after.as_long()		== 8);
+		CHECK(diffs[1].level				=="tree:name");
+		CHECK(diffs[1].before.as_string()	== "child");
+		CHECK(diffs[1].after.as_string()	== "another");
+	}
+
+
+	SUBCASE("entities")
+	{
+		struct Entity
+		{
+			string name			= "simple";
+			bool flag			= true;
+			int integer			= 42;
+
+			emap(eref(name), eref(flag), eref(integer))
+		};
+
+		auto diffs = compare::entities(Entity(), Entity { "changed", false, 42 });
+
+		CHECK(diffs.size() == 2);
+		CHECK(diffs[0].level				== "flag");
+		CHECK(diffs[0].before.as_bool()		== true);
+		CHECK(diffs[0].after.as_bool()		== false);
+		CHECK(diffs[1].level				== "name");
+		CHECK(diffs[1].before.as_string() 	== "simple");
+		CHECK(diffs[1].after.as_string()	== "changed");
+	}
+}
+
 
